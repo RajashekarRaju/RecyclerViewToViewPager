c961f6e639774aa6d05626484e37561863022a79
rajas <rajashekarraju123@gmail.com>
2020-05-07 19:45:12 +0530

    Added MVVM architecture, moved classes to packages

---
+++ app/src/main/java/com/developersbreach/recyclerviewtoviewpager/viewModel/DetailViewModel.kt
@@ -0,0 +1,8 @@
+package com.developersbreach.recyclerviewtoviewpager.viewModel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class DetailViewModel(application: Application): AndroidViewModel(application) {
+
+}
---
+++ app/src/main/java/com/developersbreach/recyclerviewtoviewpager/viewModel/ListViewModel.kt
@@ -0,0 +1,8 @@
+package com.developersbreach.recyclerviewtoviewpager.viewModel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class ListViewModel(application: Application): AndroidViewModel(application) {
+
+}
