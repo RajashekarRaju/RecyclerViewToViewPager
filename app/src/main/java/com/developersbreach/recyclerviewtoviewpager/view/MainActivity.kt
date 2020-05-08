342c96c40f4325a890ea3c1e3fb5e187adf5eaf0
rajas <rajashekarraju123@gmail.com>
2020-05-08 13:03:37 +0530

    Removed default navigation controller

--- app/src/main/java/com/developersbreach/recyclerviewtoviewpager/view/MainActivity.kt
+++ app/src/main/java/com/developersbreach/recyclerviewtoviewpager/view/MainActivity.kt
@@ -2,18 +2,12 @@ package com.developersbreach.recyclerviewtoviewpager.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
 import com.developersbreach.recyclerviewtoviewpager.R
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var mNavigationController: NavController
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        mNavigationController = Navigation.findNavController(this, R.id.nav_host_fragment)
     }
 }
