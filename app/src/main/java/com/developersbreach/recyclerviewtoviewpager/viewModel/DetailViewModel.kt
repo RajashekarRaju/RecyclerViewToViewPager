129515c3efd06eb644da77deada4cc7f2c1fc507
rajas <rajashekarraju123@gmail.com>
2020-05-08 13:15:48 +0530

    Added properties for ViewModel with factory for ViewModel instance

--- app/src/main/java/com/developersbreach/recyclerviewtoviewpager/viewModel/DetailViewModel.kt
+++ app/src/main/java/com/developersbreach/recyclerviewtoviewpager/viewModel/DetailViewModel.kt
@@ -2,7 +2,22 @@ package com.developersbreach.recyclerviewtoviewpager.viewModel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.developersbreach.recyclerviewtoviewpager.model.Sports
 
-class DetailViewModel(application: Application): AndroidViewModel(application) {
+class DetailViewModel(application: Application, sports: Sports): AndroidViewModel(application) {
 
+    private val _sportList: MutableLiveData<List<Sports>> = MutableLiveData()
+    val sportList: MutableLiveData<List<Sports>>
+        get() = _sportList
+
+    private val _selectedSport: MutableLiveData<Sports> = MutableLiveData()
+    val selectedSport: MutableLiveData<Sports>
+        get() = _selectedSport
+
+    init {
+        val sportsList: List<Sports> = Sports.sportsList(application.applicationContext)
+        _sportList.value = sportsList
+        _selectedSport.value = sports
+    }
 }
