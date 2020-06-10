e476fde41fa6c88195faa1543cd397e177783526
rajas <rajashekarraju123@gmail.com>
2020-06-10 23:47:44 +0530

    Changed to ActionBar style from NoActionBar, removed CollapsingToolbar, custom views

--- app/src/main/java/com/developersbreach/recyclerviewtoviewpager/view/detail/DetailViewPagerAdapter.kt
+++ app/src/main/java/com/developersbreach/recyclerviewtoviewpager/view/detail/DetailViewPagerAdapter.kt
@@ -5,66 +5,28 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.appcompat.widget.Toolbar
-import androidx.navigation.Navigation
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import androidx.viewpager2.widget.ViewPager2
-import androidx.viewpager2.widget.ViewPager2.*
 import com.developersbreach.recyclerviewtoviewpager.R
 import com.developersbreach.recyclerviewtoviewpager.model.Sports
 import com.developersbreach.recyclerviewtoviewpager.view.detail.DetailViewPagerAdapter.DetailViewHolder
-import com.google.android.material.appbar.AppBarLayout
-import com.google.android.material.appbar.CollapsingToolbarLayout
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
-class DetailViewPagerAdapter(private val viewPager2: ViewPager2) :
-    ListAdapter<Sports, DetailViewHolder>(DiffCallback) {
-
+class DetailViewPagerAdapter : ListAdapter<Sports, DetailViewHolder>(DiffCallback) {
 
     class DetailViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val toolbar: Toolbar = itemView.findViewById(R.id.detail_toolbar)
-        private val appBarLayout: AppBarLayout = itemView.findViewById(R.id.detail_appbar)
-        val collapsingToolbar: CollapsingToolbarLayout =
-            itemView.findViewById(R.id.detail_collapsing_toolbar)
         private val icon: ImageView = itemView.findViewById(R.id.detail_image_view)
         private val title: TextView = itemView.findViewById(R.id.title_detail_text_view)
         private val subtitle: TextView = itemView.findViewById(R.id.subtitle_detail_text_view)
         private val about: TextView = itemView.findViewById(R.id.about_detail_text_view)
-        val fab: FloatingActionButton = itemView.findViewById(R.id.fab)
 
         fun bind(sportsArgs: Sports) {
             icon.setImageResource(sportsArgs.icon)
             title.text = sportsArgs.title
             subtitle.text = sportsArgs.subtitle
             about.text = sportsArgs.about
-            appBarLayout.addOnOffsetChangedListener(appBarLayoutListener(sportsArgs))
-            toolbar.setNavigationOnClickListener { v ->
-                Navigation.findNavController(v).navigateUp()
-            }
         }
-
-        private fun appBarLayoutListener(sportsArgs: Sports): AppBarLayout.OnOffsetChangedListener =
-            object : AppBarLayout.OnOffsetChangedListener {
-                var isShow = false
-                var scrollRange = -1
-                override fun onOffsetChanged(appBarLayout: AppBarLayout, verticalOffset: Int) {
-                    if (scrollRange == -1) {
-                        scrollRange = appBarLayout.totalScrollRange
-                    }
-                    if (scrollRange + verticalOffset == 0) {
-                        // Show title when completely collapsed.
-                        collapsingToolbar.title = sportsArgs.title
-                        isShow = true
-                    } else if (isShow) {
-                        // Hide title when collapsedToolBar is completely visible using empty string.
-                        collapsingToolbar.title = ""
-                        isShow = false
-                    }
-                }
-            }
     }
 
     override fun onCreateViewHolder(
@@ -73,9 +35,7 @@ class DetailViewPagerAdapter(private val viewPager2: ViewPager2) :
     ): DetailViewHolder {
         return DetailViewHolder(
             LayoutInflater.from(parent.context).inflate(
-                R.layout.item_detail_adapter,
-                parent,
-                false
+                R.layout.item_detail_adapter, parent, false
             )
         )
     }
@@ -83,27 +43,6 @@ class DetailViewPagerAdapter(private val viewPager2: ViewPager2) :
     override fun onBindViewHolder(holder: DetailViewHolder, position: Int) {
         val sportsArgs: Sports = getItem(position)
         holder.bind(sportsArgs)
-
-        viewPager2.registerOnPageChangeCallback(object : OnPageChangeCallback() {
-            override fun onPageScrollStateChanged(state: Int) {
-                super.onPageScrollStateChanged(state)
-
-                when (state) {
-                    SCROLL_STATE_DRAGGING -> {
-                        holder.toolbar.navigationIcon = null
-                        holder.fab.visibility = View.INVISIBLE
-                    }
-                    SCROLL_STATE_IDLE -> {
-                        holder.toolbar.setNavigationIcon(R.drawable.ic_up_button)
-                        holder.fab.visibility = View.VISIBLE
-                    }
-                    SCROLL_STATE_SETTLING -> {
-                        holder.toolbar.navigationIcon = null
-                        holder.fab.visibility = View.INVISIBLE
-                    }
-                }
-            }
-        })
     }
 
     companion object DiffCallback : DiffUtil.ItemCallback<Sports>() {
