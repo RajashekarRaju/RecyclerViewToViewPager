4d57dd25fb81a0380a9277d4321b51cf856ae0df
rajas <rajashekarraju123@gmail.com>
2020-06-10 23:46:21 +0530

    Replaced ListFragment.kt with MainActivity.kt, DetailFragment.kt with DetailActivity.kt

--- app/src/main/java/com/developersbreach/recyclerviewtoviewpager/view/detail/ZoomOutPageTransformer.kt
+++ app/src/main/java/com/developersbreach/recyclerviewtoviewpager/view/detail/ZoomOutPageTransformer.kt
@@ -2,6 +2,8 @@ package com.developersbreach.recyclerviewtoviewpager.view.detail
 
 import android.view.View
 import androidx.viewpager2.widget.ViewPager2
+import kotlin.math.abs
+import kotlin.math.max
 
 private const val MIN_SCALE = 0.85f
 private const val MIN_ALPHA = 0.5f
@@ -19,7 +21,7 @@ class ZoomOutPageTransformer : ViewPager2.PageTransformer {
                 }
                 position <= 1 -> { // [-1,1]
                     // Modify the default slide transition to shrink the page as well
-                    val scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position))
+                    val scaleFactor = max(MIN_SCALE, 1 - abs(position))
                     val vertMargin = pageHeight * (1 - scaleFactor) / 2
                     val horzMargin = pageWidth * (1 - scaleFactor) / 2
                     translationX = if (position < 0) {
---
+++ app/src/main/java/com/developersbreach/recyclerviewtoviewpager/view/list/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.developersbreach.recyclerviewtoviewpager.view.list
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.developersbreach.recyclerviewtoviewpager.R
+import com.developersbreach.recyclerviewtoviewpager.model.Sports
+import com.developersbreach.recyclerviewtoviewpager.view.detail.DetailActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar!!.title = getString(R.string.list_title)
+
+        val sportsList: List<Sports> = Sports.sportsList(applicationContext)
+        val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
+
+        val adapter = SportsAdapter(sportsList, sportsItemListener)
+        recyclerView.adapter = adapter
+    }
+
+    private val sportsItemListener = SportsAdapter.OnClickListener { sports ->
+        val intent = Intent(this, DetailActivity::class.java)
+        intent.putExtra("Intent to Detail Activity", sports)
+        startActivity(intent)
+    }
+}
